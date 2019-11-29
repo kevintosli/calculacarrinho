8a9586abcf5047e548d8d5129a3ba75a0bc13405
Kevin Tosli <kevintosli@icloud.com>
2019-11-29 01:18:02 -0300

    Usage of new component CartList, total sum redesigned and CalcControls submiting new items

--- src/App.vue
+++ src/App.vue
@@ -10,17 +10,24 @@
         <div></div>
       </div>
 
-      <ul class="list-items flex-col flex-shrink-0">
-        <ListItem v-for="(item, index) in cart" class="item flex-row" :item="item" :key="index" />
-      </ul>
+      <CartList :list="cart.list" @select="toggle_selected_item" />
 
-      <div class="list-footer flex-row flex-shrink-0">
-        <div class="item-name"></div>
-        <div class="item-un">{{ list_units_total }}</div>
-        <div class="item-price">{{ list_total }}</div>
+      <div class="list-footer flex-col flex-shrink-0">
+        <div class="flex-row">
+          <div class="__label">Itens no carrinho</div>
+          <div class="__value">{{ list_units_total }}un</div>
+        </div>
+        <div class="flex-row">
+          <div class="__label">Total da compra</div>
+          <div class="__value">{{ list_total }}</div>
+        </div>
       </div>
     </div>
-    <CalcControls class="relative pin-b pin-x flex-shrink-0" />
+
+    <CalcControls
+      class="relative pin-b pin-x flex-shrink-0"
+      @submit="cart.add"
+    />
   </application>
 </template>
 
@@ -28,46 +35,61 @@
 export default {
   name: "app",
   components: {
-    ListItem: () => import("@components/ListItem"),
+    CartList: () => import("@components/CartList"),
     CalcControls: () => import("@components/CalcControls")
   },
   data() {
     return {
-      cart: [
-        {
-          name: "Leite",
-          units: 12,
-          price: 2.99
-        },
-        {
-          name: "Pão",
-          units: 3,
-          price: 4.5
-        },
-        {
-          name: "Cacetinho",
-          units: 3,
-          price: 12.87
-        },
-        {
-          name: "Bucetinha",
-          units: 2,
-          price: 0.54
+      cart: {
+        list: [
+          {
+            name: "Leite",
+            units: 12,
+            price: "2.99",
+            selected: false
+          },
+          {
+            name: "Pão",
+            units: 3,
+            price: "4.50",
+            selected: false
+          },
+          {
+            name: "Cacetinho",
+            units: 3,
+            price: "12.87",
+            selected: false
+          },
+          {
+            name: "Bucetinha",
+            units: 2,
+            price: "0.54",
+            selected: false
+          }
+        ],
+        add: $event => {
+          // window.console.log($event);
+          this.cart.list.push({
+            name: "",
+            units: $event.units,
+            price: $event.value,
+            selected: false
+          });
         }
-      ]
+      }
     };
   },
   computed: {
     list_units_total() {
       let sum = 0;
-      this.cart.forEach(item => {
+      this.cart.list.forEach(item => {
         sum += item.units;
       });
       return sum;
     },
     list_total() {
       let sum = 0;
-      this.cart.forEach(item => {
+      this.cart.list.forEach(item => {
         const item_sum = item.price * item.units;
         sum += item_sum;
       });
@@ -75,6 +97,15 @@ export default {
         style: "currency",
         currency: "BRL"
       });
+    }
+  },
+  methods: {
+    toggle_selected_item($event) {
+      const NEW_STATE = !this.cart.list[$event].selected;
+      this.cart.list.forEach(item => {
+        item.selected = false;
+      });
+      this.cart.list[$event].selected = NEW_STATE;
     }
   },
   mounted() {
@@ -120,18 +151,7 @@ application {
   text-transform: uppercase;
   @extend %graphene-font-style-meta;
 }
-.list-items {
-  padding: 0 rem(20px);
 
-  .item {
-    border-bottom: rem(1px) solid var(--color-ui-separator);
-    line-height: rem(44px);
-
-    // &:nth-child(even) {
-    //   background-color: var(--color-ui-listalt);
-    // }
-  }
-}
 .item-name {
   width: 62%;
 }
@@ -143,12 +163,24 @@ application {
   text-align: right;
   width: 30%;
 }
+
 .list-footer {
-  color: var(--color-text-placeholder);
+  background-color: var(--color-ui-separator);
+  border-radius: var(--ui-border-radius-regular);
+  color: var(--color-text-heading);
   // border-top: rem(1.6px) solid var(--color-ui-separator);
-  line-height: rem(36px);
-  font-weight: $graphene-font-weight-semibold;
+  line-height: rem(24px);
   margin: 0 var(--app-view-padding);
+  margin-top: rem(8px);
+  padding: rem(8px) rem(12px);
   @extend %graphene-font-style-paragraph;
+
+  .__label {
+    color: var(--color-text-placeholder);
+    flex-grow: 1;
+  }
+  .__value {
+    font-weight: $graphene-font-weight-semibold;
+  }
 }
 </style>
